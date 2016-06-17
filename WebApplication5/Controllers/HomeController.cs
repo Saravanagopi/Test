3098b9218d70a1c6f372a7142272b47c3373a5a2
saravanagopi <ssgopi1983@yahoo.co.in>
2016-06-17 15:50:35 +0530

    test

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            TestModel TM = new TestModel();
+            string str = TM.Test("Testing");
+            return str;
+        }
+    }
+}
---
+++ WebApplication5/Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class TestModel
+    {
+        public string Test(string Param)
+        {
+            return Param;
+        }
+    }
+}
